b937f1476cc25f65d5c99328aedc7f872343cd29
Gabriel Ramalho <168201242+GabrielLemoz@users.noreply.github.com>
2025-02-27 00:18:01 -0300

    EXEMPLO DE SCANF E PRINTF
    A função scanf é ideal para ler dados formatados, como inteiros, diretamente do teclado, enquanto a função printf é utilizada para imprimir mensagens formatadas na tela

---
+++ main (1).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    //declara uma variavel para armazenar número
+    int numero;
+
+    //pede ao usuario para digitar um numero
+        printf("digite um numero inteiro: ");
+
+    //le o numero digitado pelo usuario 
+    scanf("%d", &numero);
+
+    // exibe
+    printf("voce digitou: %d\n", numero);
+
+    return 0;
+}
