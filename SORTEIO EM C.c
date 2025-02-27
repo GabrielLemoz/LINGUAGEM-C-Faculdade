c6afd418b1954e140cc1805325206cb8e94d957e
Gabriel Ramalho <168201242+GabrielLemoz@users.noreply.github.com>
2025-02-26 22:45:57 -0300

    Add files via upload

---
+++ SORTEIO EM C.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    srand(time(NULL));
+    int sorteio = rand()%5;
+    printf("o sorteado foi o número:%d\n", sorteio);
+    return 0;
+}
